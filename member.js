b9b744405120d168a0797572ddcf333284fad31b
juann227 <juanjoruizt@gmail.com>
2025-02-20 21:25:04 -0500

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  var skills = [
+    {
+      name: 'HTML',
+      level: 'expert'
+    },
+    {
+      name: 'CSS',
+      level: 'intermediate'
+    }
+  ];
+  return skills;
+}
